145f56e7de403ac1254c312f0f11dd86725d3e9e
Aniket Pradhan <aniketpradhan814@gmail.com>
2023-04-11 22:10:18 +0530

    sidebar toggle completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,44 @@
-import React from 'react'
-import {GiHamburgerMenu} from "react-icons/gi"
-import {FaUser} from "react-icons/fa"
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaUser } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/redux/slices/appSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
   return (
-    <div className='grid grid-flow-col p-3 shadow-lg mb-2'>
+    <div className="grid grid-flow-col p-3 shadow-lg mb-2">
       {/* Left Logo */}
-      <div className='flex justify-start items-center col-span-1'>
-        <GiHamburgerMenu/>
-        <div className='mx-3'>
+      <div className="flex justify-start items-center col-span-1">
+        <GiHamburgerMenu
+          onClick={() => toggleMenuHandler()}
+          className="cursor-pointer"
+        />
+        <div className="mx-3">
           <span>WATCHFLIX</span>
         </div>
       </div>
       {/* Middle Search */}
-      <div className='col-span-10'>
-        <input className='w-1/2 border border-gray-400 rounded-l-full p-2' type='text' />
-        <button className='border border-gray-400 rounded-r-full p-2'>Search</button>
+      <div className="col-span-10">
+        <input
+          className="w-1/2 border border-gray-400 rounded-l-full p-2"
+          type="text"
+        />
+        <button className="border border-gray-400 rounded-r-full p-2">
+          Search
+        </button>
       </div>
       {/* Right Profile */}
-      <div className='flex justify-start items-center col-span-1'>
-        <FaUser size="1.7em"/>
+      <div className="flex justify-start items-center col-span-1">
+        <FaUser size="1.7em" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const SideBar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  if (!isMenuOpen) return null;
+
   return (
-    <div className='p-5 shadow-md w-52 h-[90vh]'>
+    <div className="p-5 shadow-md w-52 h-[88vh]">
       <ul>
         <li>Home</li>
         <li>Sorts</li>
         <li>Live</li>
         <li>Game</li>
       </ul>
-      <br/>
-      <h1 className='font-bold'>Subscription</h1>
+      <br />
+      <h1 className="font-bold">Subscription</h1>
       <ul>
         <li>Music</li>
         <li>Gaming</li>
         <li>Sports</li>
         <li>Movies</li>
       </ul>
-      <br/>
-      <h1 className='font-bold'>Watch Later</h1>
+      <br />
+      <h1 className="font-bold">Watch Later</h1>
       <ul>
         <li>Music</li>
         <li>Gaming</li>
@@ -26,7 +31,7 @@ const SideBar = () => {
         <li>Movies</li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
